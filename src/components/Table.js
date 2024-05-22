80184cd07b58912346d5349b96f3cd759324399d
kiran76 <ksknew76@gmail.com>
2024-05-22 20:10:20 +0530

    created table

---
+++ src/components/Table.js
@@ -0,0 +1,18 @@
+const Table = () =>{
+    return(
+        <>
+        <table className="m-[5rem] mx-auto rounded-[2rem] border-gray-500 max-w-[80rem] overflow-hidden">
+            <thead>
+                <tr>
+                    <th className="min-w-[30rem] p-[1.5rem]">ratings_average</th>
+                    <th className="min-w-[30rem] p-[1.5rem]">ratings_average</th>
+                    <th className="min-w-[30rem] p-[1.5rem]">ratings_average</th>
+                    <th className="min-w-[30rem] p-[1.5rem]">ratings_average</th>
+                </tr>
+            </thead>
+        </table>
+        </>
+    )
+}
+
+export default Table;
