f5360cca2ac62c17c341a76cb6c82f06c2ea69f9
Korada Saikiran <80874246+saikiran76@users.noreply.github.com>
2024-05-24 00:44:28 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Search from './components/SearchForm';
 import Table from './components/Table';
 
 function App() {
   return (
     <div className="App m-16 font-poppins">
-      {/* <Search/> */}
       <Table/>
     </div>
   );
