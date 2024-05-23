cc8c82e2562d2668a92b86523f55af2127d72b14
Korada Saikiran <80874246+saikiran76@users.noreply.github.com>
2024-05-24 01:18:02 +0530

    added Loader

---
+++ src/utils/Loader.js
@@ -0,0 +1,12 @@
+export const Loader = () =>{
+    return(
+        <div className="flex justify-center ml-[9em] items-center h-screen">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-
+            purple-800 flex flex-col"></div>
+                <h1 className="text-3xl font-bold text-purple-800">Loading...</h1>
+            <div className="animate-pulse h-16 w-16 rounded-full bg-purple-800"></div>
+        </div>
+
+    )
+}
+
